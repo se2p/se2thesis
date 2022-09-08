837743e1e2673fd2f242271f6167bda7d6678303
Stephan Lukasczyk <stephan@lukasczyk.me>
2022-09-08 09:42:49 +0200

    Add a l3build configuration

---
+++ build.lua
@@ -0,0 +1,32 @@
+-- Build script for "se2thesis" files
+
+-- Identify the bundle and module
+bundle = ""
+module = "se2thesis"
+
+-- Release a TDS-style zip
+packtdszip = true
+
+-- Typeset only the .tex files
+-- typesetfiles = {"*.tex"}
+
+-- Sources files cover those for rollback
+sourcesfiles =
+  {
+    "se2thesis.dtx",
+    "se2thesis.ins",
+  }
+
+installfiles = {"*.cls", "*.sty", "*.trsl"}
+
+-- ASCII mangling is not useful for us
+asciiengines = { }
+
+-- Two runs for label testing
+checkruns = 2
+
+-- Use LuaLaTeX for compiling
+typesetexe = "lualatex"
+
+-- Set up test runners
+checkengines = {"pdftex", "luatex"}
