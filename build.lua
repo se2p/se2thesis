0e71606d9c999e96517c487a14a7b886b09d059a
Stephan Lukasczyk <stephan@lukasczyk.me>
2022-09-12 13:45:55 +0200

    Provide an automated version update to build.lua

--- build.lua
+++ build.lua
@@ -35,3 +35,46 @@ typesetexe = "lualatex"
 
 -- Set up test runners
 checkengines = {"pdftex", "luatex"}
+
+-- Detail how to set the version automatically
+function update_tag(file, content, tagname, tagdate)
+  tagname = string.gsub(tagname, "^v", "")
+  dict_tagdate = string.gsub(tagdate, "-", "/")
+
+  if string.match(file, "CHANGELOG.md") then
+    local pattern = "v%d%.%d.%d%?"
+    local url = "https://github.com/se2p/se2thesis/compare/"
+    local previous = string.match(content, "compare/(" .. pattern .. ")%.%.%.HEAD")
+    if tagname == previous then return content end
+    content = string.gsub(content,
+      "## %[Unreleased%]",
+      "## [Unreleased]\n\n## [v" .. tagname .. "] - " .. tagdate)
+    return string.gsub(content,
+      pattern .. "%.%.%.HEAD",
+      "v" .. tagname .. "...HEAD\n[v" .. tagname .. "]: " .. url .. previous
+        .. "...v" .. tagname)
+  end
+  if string.match(file, "%.dtx$") then
+    content = string.gsub(content,
+      "\n\\ProvidesExplClass %{se2thesis%} %{[^}]+%} %{[^}]+%}\n",
+      "\n\\ProvidesExplClass {se2thesis} {"
+        .. tagdate .. "} {" .. tagname .. "}\n")
+    content = string.gsub(content,
+      "\n\\ProvidesExplPackage %{se2colors%} %{[^}]+%} %{[^}]+%}\n",
+      "\n\\ProvidesExplPackage {se2colors} {"
+        .. tagdate .. "} {" .. tagname .. "}\n")
+    content = string.gsub(content,
+      "\n\\ProvidesExplPackage %{se2fonts%} %{[^}]+%} %{[^}]+%}\n",
+      "\n\\ProvidesExplPackage {se2fonts} {"
+        .. tagdate .. "} {" .. tagname .. "}\n")
+    content = string.gsub(content,
+      "\n\\ProvideDictionaryFor%{English%}%{se2translations%}%[[^]]+%]\n",
+      "\n\\ProvideDictionaryFor{English}{se2translations}["
+        .. dict_tagdate .. "]\n")
+    content = string.gsub(content,
+      "\n\\ProvideDictionaryFor%{German%}%{se2translations%}%[[^]]+%]\n",
+      "\n\\ProvideDictionaryFor{German}{se2translations}["
+        .. dict_tagdate .. "]\n")
+  end
+  return content
+end
