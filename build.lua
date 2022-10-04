15a8d5136db59e52c8bbc8107f1ca4520530d7a5
Stephan Lukasczyk <stephan@lukasczyk.me>
2022-10-04 14:05:39 +0200

    Add an upload config for l3build

--- build.lua
+++ build.lua
@@ -78,3 +78,17 @@ function update_tag(file, content, tagname, tagdate)
   end
   return content
 end
+
+uploadconfig = {
+  author      = "Stephan Lukasczyk",
+  license     = "lppl1.3c",
+  summary     = "A Thesis Class for the Chair of Software Engineering II at the University of Passau, Germany",
+  topic       = {"class", "dissertation", "expl3"},
+  ctanPath    = "/macros/latex/contrib/se2thesis",
+  repository  = "https://github.com/se2p/se2thesis",
+  bugtracker  = "https://github.com/se2p/se2thesis/issues",
+  update      = true,
+  description = [[
+The se2thesis bundle provides a document class for writing a thesis with the [Chair of Software Engineering II](https://www.fim.uni-passau.de/en/chair-for-software-engineering-ii/) at the [University of Passau](https://www.uni-passau.de), Germany.  The class is based on Markus Kohm's [KOMA-Script](https://www.ctan.org/pkg/koma-script) classes and provides several additions and customizations to these classes.  While the class provides some basic settings, mostly regarding the type area, fonts, and the title page, it still provides large degrees of freedom to its users.  However, the package's documentation also provides recommendations regarding several aspects, for example, recommending [BibLaTeX](https://www.ctan.org/pkg/biblatex) for bibliographies.
+  ]]
+}
